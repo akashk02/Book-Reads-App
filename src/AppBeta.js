4468137433a748c8e19e50901bcafed23164db15
akash <akashsinghk02@gmail.com>
2019-04-15 07:23:04 +0530

    Added Route And Search Page

--- src/AppBeta.js
+++ src/AppBeta.js
@@ -3,85 +3,105 @@ import React, { Component } from 'react'
 import './App.css'
 import * as BooksApi from './BooksAPI'
 import BooksList from './BooksList'
+import { Route, Link } from 'react-router-dom'
+import SearchBooks from './SearchBooks'
 
 class BooksApp extends Component {
 
-    state = {
-        books: []
-    };
+  state = {
+    books: []
+  };
 
 
 
-    componentDidMount() {
-        BooksApi.getAll().then(books => (this.setState({ books })));
-    }
+  componentDidMount() {
+    BooksApi.getAll().then(books => (this.setState({ books })));
+  }
 
-    moveBooks = (book, shelf) => {
-        console.log(`moveBooks books = ${book} shelf=${shelf} `)
-        BooksApi.update(book, shelf).then(data => {
-            console.log('update api data =', data)
-            BooksApi.getAll().then(books => (this.setState({ books })));
-        })
+  moveBooks = (book, shelf) => {
+    console.log(`moveBooks books = ${book} shelf=${shelf} `)
+    BooksApi.update(book, shelf).then(data => {
+      console.log('update api data =', data)
+      BooksApi.getAll().then(books => (this.setState({ books })));
+    })
 
-    }
+  }
 
-    render() {
+  render() {
 
-        let currentlyReading = this.state.books.filter(book => book.shelf === "currentlyReading");
-        let wantToRead = this.state.books.filter(book => book.shelf === "wantToRead");
-        let read = this.state.books.filter(book => book.shelf === "read");
+    let currentlyReading = this.state.books.filter(book => book.shelf === "currentlyReading");
+    let wantToRead = this.state.books.filter(book => book.shelf === "wantToRead");
+    let read = this.state.books.filter(book => book.shelf === "read");
 
-        return (
+    return (
 
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-
-                    <div>
-                        <BooksList
-                            books={currentlyReading}
-                            shelf='Currently Reading'
-                            moveBookFromShelfs={this.moveBooks}
-                            optionValue={this.currentlyReading && this.currentlyReading[0].shelf}
-
-                        ></BooksList>
+      <div className="list-books">
 
 
 
-                        <BooksList
-                            books={wantToRead}
-                            shelf='Want To Read'
-                            moveBookFromShelfs={this.moveBooks}
-                            optionValue={this.wantToRead && this.wantToRead[0].shelf}
-                        >
+        <Route exact path="/" render={() => (
 
-                        </BooksList>
-
-                        <BooksList
-                            books={read}
-                            shelf='Read'
-                            moveBookFromShelfs={this.moveBooks}
-                            optionValue={this.read && this.read[0].shelf}
-
-                        >
-
-                        </BooksList>
+          <div className="list-books-content">
 
 
-                    </div>
+            <div>
 
-                </div>
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+
+              <BooksList
+                books={currentlyReading}
+                shelf='Currently Reading'
+                moveBookFromShelfs={this.moveBooks}
+                optionValue={this.currentlyReading && this.currentlyReading[0].shelf}
+              ></BooksList>
+
+
+
+              <BooksList
+                books={wantToRead}
+                shelf='Want To Read'
+                moveBookFromShelfs={this.moveBooks}
+                optionValue={this.wantToRead && this.wantToRead[0].shelf}>
+              </BooksList>
+
+              <BooksList
+                books={read}
+                shelf='Read'
+                moveBookFromShelfs={this.moveBooks}
+                optionValue={this.read && this.read[0].shelf}>
+              </BooksList>
+
+
+              <div className="open-search">
+                <Link className="open-search-link" to="/searchBooks">search</Link>
+              </div>
+
             </div>
+          </div>
+
+        )} >
+        </Route>
+
+        <Route path="/searchBooks" render={() => (
+          <SearchBooks>
+          </SearchBooks>
+        )}>
+        </Route>
 
 
 
 
-        )
+      </div>
 
 
-    }
+
+
+    )
+
+
+  }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import AppBeta from './AppBeta'
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 
 
-ReactDOM.render(<AppBeta />, document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter><AppBeta /></BrowserRouter>
+    , document.getElementById('root'))
+serviceWorker.register();
+
